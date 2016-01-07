de6268ae6142c74a2a0967893be40c2e77a4310b
Casper Co <haln22111@gmail.com>
2016-01-07 11:10:13 -0800

    Creating index.php page
    My first page for the lab

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="content-Type" content="text/html; charset=UTF-8">
+  <title>ACIT4850 Lab 1 - Casper Co (A00932505)</title>
+</head>
+<body>
+  <?php
+    echo 'Hello everybody!';
+  ?>
+</body>
+</html>
